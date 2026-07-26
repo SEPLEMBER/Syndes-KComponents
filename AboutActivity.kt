a1734b4be4b38600dd5da8cb8d4121cd95e26402
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2026-07-26 22:14:07 +0700

    Add files via upload

---
+++ AboutActivity.kt
@@ -0,0 +1,12 @@
+package org.syndes.kotlincomponents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
